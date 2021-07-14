2de446f73519e2125ca05f3c0231ad0e20e05d86
betalevrai <83734866+betalevrai@users.noreply.github.com>
2021-07-14 02:43:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Statistiques globales : \n\nLe bot a un total de ${client.guilds.cache.size} serveurs.`) 
+    console.log(`le bot s'appelle ${client.user.tag}!`);
+    console.log(`                                           le bot est ok il est bien en ligne !`);
+    console.log(`    | logs du bot |`);
+  });
+
+
+
+client.on('message',  message => {
+    function is_url(str) {
+      let regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+      let regx = /^((?:https?:)?\/\/)?((?:www|m)\.)? ((?:discord\.gg|discordapp\.com))/g
+    let cdu = regx.test(message.content.toLowerCase().replace(/\s+/g, ''))
+      if(regexp.test(str)) {
+        return true;
+      } else {
+        return false;
+      }
+      
+      if(regx.test(str)) {
+        return true;
+      } else {
+        return false;
+      }
+    
+    }
+     if(is_url(message.content) === true) {
+         
+        }
+        if(is_url(message.content) === true) {
+          message.delete()
+          console.log (`${message.author.tag} à essayé de posté un liens sont message etait = "${message.content}" `)
+          //return message.channel.send("Les liens ne sont pas autorisé !")
+        }
+});
+
+
+client.login(process.env.TOKEN);
